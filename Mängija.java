227dbd7b16734204d5f576648d9bb350d065a3b7
elszzz <teeletarvis123@gmail.com>
2025-03-26 17:09:08 +0200

    test3

---
+++ Mängija.java
@@ -0,0 +1,9 @@
+public class Mängija implements Ese{
+    private byte elupunktid;
+    private int[] positsioon;
+
+    public Mängija(byte elupunktid, int[] positsioon) {
+        this.elupunktid = elupunktid;
+        this.positsioon = positsioon;
+    }
+}
---
+++ MängijaA.java
@@ -0,0 +1,7 @@
+public class MängijaA extends Mängija{
+
+    public MängijaA(byte elupunktid, int[] positsioon) {
+        super((byte) 50, new int[]{0, 0});
+    }
+
+}
